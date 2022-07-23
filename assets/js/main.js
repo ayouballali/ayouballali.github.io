ce7533675574dec1c612832457bf50a16706bd30
ayouballali <ayoubralali@gmail.com>
2022-07-23 16:21:52 +0100

    the testimonilal section

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,7 @@ modalCloses.forEach((modalClose)=>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPoertfolio = new Swiper(".portfolio__container", {
    cssMode: true,
    loop:true,
 
@@ -110,7 +110,25 @@ let swiper = new Swiper(".portfolio__container", {
  });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestmonial = new Swiper(".testimonial__container", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween:48,
 
+  
+   pagination: {
+     el: ".swiper-pagination",
+     clickable:true,
+     dynamicBullets:true,
+   },
+   breakpoints:{
+      568:{
+         slidesPerView:2,
+         
+      }
+   }
+  
+ });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
