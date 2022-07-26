6610d0dc5e7d3eea5cb670338ad93f5f553140b3
ayouballali <ayoubralali@gmail.com>
2022-07-26 15:48:35 +0100

    scrol part and styling links in the header

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,7 +131,27 @@ let swiperTestmonial = new Swiper(".testimonial__container", {
  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/* this code is really intressting just copy paste */
+const sections = document.querySelectorAll('section[id]')
 
+console.log(sections);
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
